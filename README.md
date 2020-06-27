5d4d15e4d71c6f930a477b059030d8fe7a26b403
chedieck <loboestevao@gmail.com>
2020-06-27 18:33:37 -0300

    Stable with no repeated mutagens on the same pass

--- README.md
+++ README.md
@@ -25,5 +25,9 @@ optional arguments:
                         output file to write wordlist.
 ```
 
+Repository with example files `words.test` and `dates.test` for usage example: `python passbiaser.py 8 12 -w words.test -d dates.test -c -o temp.txt`
+
+The script doesn't use the same string with and without mutagen in the same password. So, if you have the words `zé` and `maria` with the `-c` mutagen, you may get `Zémaria, zémaria, zéMaria...` but  not `zéZé, mariaMaria`.
+
 
 This project is for educational purpuses only. Use at your own discretion, the contributors should not be hold responsible for any illegal activities performed with this software.
--- passbiaser.py
+++ passbiaser.py
@@ -12,7 +12,9 @@ def init_argparser():
     parser.add_argument('-u', '--upper', action='store_true', help='uppercase mutagen')
     parser.add_argument('--leet', action='store_true', help='l33t mutagen')
     parser.add_argument('--leetat', action='store_true', help='l33t mutagen with aA -> @')
-    parser.add_argument('-c', '--captalize', action='store_true', help='capitalize mutagen')
+    parser.add_argument('-c', '--capitalize', action='store_true', help='capitalize mutagen')
+    parser.add_argument('--compress-md', action='store_true', help='compress months and days starting with 0 mutagen, e.g 02 -> 2')
+    parser.add_argument('--compress-y', action='store_true', help='compress years from 4 digits to 2 mutagen, e.g 1978 -> 78')
     parser.add_argument('-w', '--wordsfile', help='file containing the words to be used.') 
     parser.add_argument('-d', '--datesfile', help='file containing the dates to be used, each line should be in DD-MM-YYYY\n'
             'format, e.g: 16-04-1994; xx-04-1995; 16-xx-xxxx; etc')
@@ -21,36 +23,12 @@ def init_argparser():
     args = parser.parse_args()
     return args
 
-#words = ['zezin', 'totó']
-#dates = ['26-10-1995', '31-09-1800']
-
 
 
 def gen_for_N(N, args, words, dates):
     mandatorywords = [w for w in words if w[0] == '!']
     words = [w if w[0] != '\\' else w for w in words]
     pass
-
-def parse_words (args, opened_file):
-    words = opened_file.read().splitlines()
-    input_list = words.copy()
-    for word in words :
-        if args.captalize:
-            input_list.append(word.capitalize())
-    words = input_list.copy()
-    for word in words :
-        if args.upper:
-            input_list.append(word.upper())
-    words = input_list.copy()
-    for word in words :
-        if args.leet:
-            input_list.append(w := word.translate(LEET_MAP))
-    words = input_list.copy()
-    for word in words :
-        if args.leetat:
-            input_list.append(word.translate(LEETat_MAP))
-
-    return input_list
 
 def parse_dates (opened_file):
     dates = opened_file.read().splitlines()
@@ -65,37 +43,40 @@ def _combination_len(combination_set):
     return sum([len(x) for x  in combination_set])
 
 
-#hard way, fuckit
-def combinations(S, minL, maxL, curr_comb=set(), ret=[]):
-    for s in S: 
-        #####this#section#parses#dates####
-        special = '' # string to specify if current s is day or year
-        N = 1 #auxiliar counter
-        if s.isdigit():     # digits will be treated in a special way
-            if len(s) == 4: # 4-sized digit string will be interpreted as a year
-                special = 'y' 
-            if len(s) == 2 and s[0] == '0':
+#TODo minL maxL
+def combinations(args, S, minL, maxL, curr_comb=set(), ret=[]):
+    """This is a very verbose funcion. The first part parses the mutagens,
+    while only the second actually calculates the combinations. 
+    """
+    for raw_s in S: 
+        #####this section parses dates####
+        s_mutations = {raw_s}
+        if raw_s.isdigit() and (args.compress_md or args.compress_y):
+            if len(raw_s) == 4 and args.compress_y: # 4-sized digit string will be interpreted as a year
+                s_mutations.add(raw_s[-2:])
+            if len(raw_s) == 2 and raw_s[0] == '0' and args.compress_md:
                 # 2-sized digit string starting with 0 will be interpreted as a month or day
-                special = 'd' 
-            N = 2
-        for i in range(N):
-            real_s = s  
-            if i == 1:     # second case where s is a year, month or day
-                if special == 'y':
-                    s = s[-2:] # get only last two digits to represent year.
-                if special == 'd':
-                    s = s[1] # get s without the initial zero
-            ##################################
-
+                s_mutations.add(raw_s[1])
+        if (not raw_s.isdigit()) and (args.leet or args.leetat or args.upper or args.capitalize):
+            if args.leet:
+                s_mutations.add(raw_s.translate(LEET_MAP))
+            if args.leetat:
+                s_mutations = s_mutations | set([i.translate(LEETAT_MAP) for i in s_mutations])
+            if args.upper:
+                s_mutations = s_mutations | set([i.upper() for i in s_mutations])
+            if args.capitalize:
+                s_mutations = s_mutations | set([i.capitalize() for i in s_mutations])
+        ######
+        for s in s_mutations:
             new_comb = curr_comb.union({s})
-            newS = S.difference(curr_comb.union({real_s}))
+            newS = S.difference(curr_comb.union({raw_s}))
 
-            auxL =_combination_len(curr_comb) + len(s) 
+            auxL = _combination_len(new_comb)
             if minL <= auxL <= maxL:
                 ret.append(new_comb)
-                ret = combinations(newS, minL, maxL, new_comb, ret=ret)
+                ret = combinations(args, newS, minL, maxL, new_comb, ret=ret)
             elif auxL < minL:
-                ret = combinations(newS, minL, maxL, new_comb, ret=ret)
+                ret = combinations(args, newS, minL, maxL, new_comb, ret=ret)
     return ret
 
 
@@ -110,7 +91,7 @@ def arranjos(combinations):
 def main(args):
     if args.wordsfile:
         with open(args.wordsfile, 'r') as f:
-            words = parse_words(args, f)
+            words = f.read().splitlines()
             assert len(max(words, key=len)) <= args.maxLen, "Wordsfile incompatible with maxLen."
     if args.datesfile:
         with open(args.datesfile, 'r') as f:
@@ -119,6 +100,7 @@ def main(args):
     fullset = set(words + dates)
     fullset.remove('')
 
+    A = arranjos(combinations(args, fullset, args.minLen, args.maxLen))
     if args.outputfile:
         with open(args.outputfile, 'w') as f:
             f.write('\n'.join(A))
